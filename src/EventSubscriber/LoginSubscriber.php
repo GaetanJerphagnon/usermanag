14c0e61803a93a4f5b34cbdd5e5e7a02c30277cf
GaetanJerphagnon <jerphagnon.gaetan@gmail.com>
2021-03-26 10:31:47 +0100

    Add loginSubscriber for lastConnectedAt + minor typo fix + remove lastConnectedAt for fixtures

---
+++ src/EventSubscriber/LoginSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class LoginSubscriber implements EventSubscriberInterface
+{
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+        
+        if ($user instanceof \App\Entity\User){
+            $user->setLastConnectedAt(new \DateTime());
+            $this->manager->persist($user);
+            $this->manager->flush();
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'security.interactive_login' => 'onSecurityInteractiveLogin',
+        ];
+    }
+}
